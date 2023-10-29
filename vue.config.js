114dcf7deb267a9d3575ff46d09958f48b970df1
luna0527 <snchen0527@163.com>
2023-10-29 19:58:07 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -4,3 +4,25 @@ module.exports = defineConfig({
   devServer:{
     port: 8081  } 
 })
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//     port: 8081,
+//     proxy: {
+//       "/api/": {
+//         pathRewrite: { // 路径改写规则
+//           '^/api': '' // 以/api/为开头的改写为'/'
+//         },
+//         target: 'http://159.223.50.155',
+//         changeOrigin: true, // 跨域访问设置，true代表跨域
+//         ws: true,
+//         'secure': false, // false为http访问，true为https访问
+//       },
+//     },
+//   },
+// })
+
+// // 添加日志消息
+// console.log("Vue CLI proxy configuration loaded."); 
